4f5db8f552ebce3075a2891afc3eba840a19d8b9
kwonsye <kwonsye@naver.com>
2020-03-18 16:33:54 +0900

    [First-commit] - gradle settings - 패키지 구조

---
+++ src/main/java/team_mic/here_and_there/backend/HereAndThereApplication.java
@@ -0,0 +1,11 @@
+package team_mic.here_and_there.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereAndThereApplication {
+    public static void main(String[] args){
+        SpringApplication.run(HereAndThereApplication.class, args);
+    }
+}
