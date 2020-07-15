8c7292bda82e66cf310f3c2b366c9c34e05ab71c
kwonsye <kwonsye@naver.com>
2020-07-15 16:10:50 +0900

    [스웨거 doc 설정 및 문서 작성] - 오디오가이드 컨트롤러 - 여행팁 컨트롤러 - 덤프 데이터 추가 컨트롤러는 @ApiIgnore

--- src/main/java/team_mic/here_and_there/backend/audio_guide/domain/repository/AudioGuideRepository.java
+++ src/main/java/team_mic/here_and_there/backend/audio_guide/domain/repository/AudioGuideRepository.java
@@ -1,7 +1,6 @@
 package team_mic.here_and_there.backend.audio_guide.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import team_mic.here_and_there.backend.audio_guide.domain.entity.AudioGuide;
 
--- src/main/java/team_mic/here_and_there/backend/audio_guide/dto/response/ResAudioGuideItemDto.java
+++ src/main/java/team_mic/here_and_there/backend/audio_guide/dto/response/ResAudioGuideItemDto.java
@@ -1,5 +1,6 @@
 package team_mic.here_and_there.backend.audio_guide.dto.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,14 +9,19 @@ import java.util.List;
 @Getter
 public class ResAudioGuideItemDto {
 
+    @ApiModelProperty(notes = "오디오 가이드 id")
     private Long audioGuideId;
 
+    @ApiModelProperty(notes = "오디오 가이드 메인 이미지 url")
     private String imageUrl;
 
+    @ApiModelProperty(notes = "오디오 가이드 제목")
     private String title;
 
+    @ApiModelProperty(notes = "오디오 가이드의 태그들")
     private List<String> tags;
 
+    @ApiModelProperty(notes = "오디오 가이드 오디오파일 url(테스트 필요)")
     private List<String> audioFileUrl;
 
     @Builder
