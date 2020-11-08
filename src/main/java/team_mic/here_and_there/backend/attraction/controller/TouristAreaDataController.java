3e11dd979dcaa942981c7d45bac59ade988f1962
kwonsye <kwonsye@naver.com>
2020-11-08 17:08:45 +0900

    [Tourist Area] 지역 상단 이미지 데이터 삽입 Controller / Service - attraction 지여에 해당하는 각각의 이미지, 지역명 데이터 한/영 삽입 - TODO : 데이터를 insert 할때 재활용가능하도록 함수 분리 리팩토링 작업 - TODO : 데이터를 insert 하기 전에 동일한 데이터가 db 에 있는지 체크하고 만약 존재한다면, exception throw 하기 - issue #38

---
+++ src/main/java/team_mic/here_and_there/backend/attraction/controller/TouristAreaDataController.java
@@ -0,0 +1,34 @@
+package team_mic.here_and_there.backend.attraction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+import team_mic.here_and_there.backend.attraction.service.TouristAreaDataService;
+
+@RequiredArgsConstructor
+@RestController
+public class TouristAreaDataController {
+
+    private final TouristAreaDataService touristAreaDataService;
+
+    @ApiIgnore
+    @PostMapping("/data/tourist-areas/images/kor")
+    public ResponseEntity<Void> insertKoreanTouristAreaImages(){
+
+        touristAreaDataService.insertKoreanTouristAreaImages();
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @ApiIgnore
+    @PostMapping("/data/tourist-areas/images/eng")
+    public ResponseEntity<Void> insertEnglishTouristAreaImages(){
+
+        touristAreaDataService.insertEnglishTouristAreaImages();
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
