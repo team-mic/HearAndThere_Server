f3ab70c305b3a3fcd9c84a0c05017e612aa4d7cf
kwonsye <kwonsye@naver.com>
2020-11-08 17:02:54 +0900

    [AudioCourse] 오디오코스 데이터 삽입 Controller / Service - 가이드 13개에 해당하는 각각의 코스 데이터 한/영 삽입 - TODO : 데이터를 insert 할때 재활용가능하도록 함수 분리 리팩토링 작업 - TODO : 데이터를 insert 하기 전에 동일한 데이터가 db 에 있는지 체크하고 만약 존재한다면, exception throw 하기 - issue #38

--- src/main/java/team_mic/here_and_there/backend/audio_course/controller/AudioCourseDataController.java
+++ src/main/java/team_mic/here_and_there/backend/audio_course/controller/AudioCourseDataController.java
@@ -14,13 +14,102 @@ public class AudioCourseDataController {
 
   private final AudioCourseDataService audioCourseDataService;
 
-  /*
   @ApiIgnore
-  @PostMapping("/audio-courses/bukchon")
+  @PostMapping("/data/audio-courses/gongneung")
+  public ResponseEntity<Void> insertGongneungCourses(){
+
+    audioCourseDataService.insertGongneungCourses();
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @ApiIgnore
+  @PostMapping("/data/audio-courses/namsan")
+  public ResponseEntity<Void> insertNamsanCourses(){
+
+    audioCourseDataService.insertNamsanCourses();
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @ApiIgnore
+  @PostMapping("/data/audio-courses/noryangjin")
+  public ResponseEntity<Void> insertNoryangjinCourses(){
+
+    audioCourseDataService.insertNoryangjinCourses();
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @ApiIgnore
+  @PostMapping("/data/audio-courses/daehakro")
+  public ResponseEntity<Void> insertDaehakroCourses(){
+
+    audioCourseDataService.insertDaehakroCourses();
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @ApiIgnore
+  @PostMapping("/data/audio-courses/dongdaemun")
+  public ResponseEntity<Void> insertDongdaemunCourses(){
+
+    audioCourseDataService.insertDongdaemunCourses();
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @ApiIgnore
+  @PostMapping("/data/audio-courses/mangwon")
+  public ResponseEntity<Void> insertMangwonCourses(){
+
+    audioCourseDataService.insertMangwonCourses();
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @ApiIgnore
+  @PostMapping("/data/audio-courses/bukchon")
   public ResponseEntity<Void> insertBukchonCourses(){
 
     audioCourseDataService.insertBukchonCourses();
 
     return ResponseEntity.status(HttpStatus.OK).build();
-  }*/
+  }
+
+  @ApiIgnore
+  @PostMapping("/data/audio-courses/jamsil")
+  public ResponseEntity<Void> insertJamsilCourses(){
+
+    audioCourseDataService.insertJamsilCourses();
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @ApiIgnore
+  @PostMapping("/data/audio-courses/yongsan")
+  public ResponseEntity<Void> insertYongSanCourses(){
+
+    audioCourseDataService.insertYongSanCourses();
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @ApiIgnore
+  @PostMapping("/data/audio-courses/olympic-park")
+  public ResponseEntity<Void> insertOlympicParkCourses(){
+
+    audioCourseDataService.insertOlympicParkCourses();
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @ApiIgnore
+  @PostMapping("/data/audio-courses/insadong")
+  public ResponseEntity<Void> insertInsadongCourses(){
+
+    audioCourseDataService.insertInsadongCourses();
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
 }
--- src/main/java/team_mic/here_and_there/backend/audio_course/domain/repository/AudioCourseElementLanguageContentRepository.java
+++ src/main/java/team_mic/here_and_there/backend/audio_course/domain/repository/AudioCourseElementLanguageContentRepository.java
@@ -1,9 +1,11 @@
 package team_mic.here_and_there.backend.audio_course.domain.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team_mic.here_and_there.backend.audio_course.domain.entity.AudioCourseElementLanguageContent;
 
 @Repository
 public interface AudioCourseElementLanguageContentRepository extends JpaRepository<AudioCourseElementLanguageContent, Long> {
+  Optional<AudioCourseElementLanguageContent> findByTitle(String title);
 }
