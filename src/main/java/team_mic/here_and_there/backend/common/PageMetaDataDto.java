6bf69ed9e862583bdd8da23952f50c4d08e6ab66
kwonsye <kwonsye@naver.com>
2021-11-10 20:32:21 +0900

    [Doc] Response Dto 의 멤버별 설명 추가

--- src/main/java/team_mic/here_and_there/backend/common/PageMetaDataDto.java
+++ src/main/java/team_mic/here_and_there/backend/common/PageMetaDataDto.java
@@ -1,13 +1,20 @@
 package team_mic.here_and_there.backend.common;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@JsonPropertyOrder({"currentPageSize", "currentPageNumber", "totalPageNumbers", "totalElements"})
 public class PageMetaDataDto {
+  @ApiModelProperty(notes = "요청한 현재 페이지 크기")
   private Integer currentPageSize;
+  @ApiModelProperty(notes = "요청한 현재 페이지 번호")
   private Integer currentPageNumber;
+  @ApiModelProperty(notes = "currentPageSize 로 동일하게 요청했을 경우 데이터가 존재하는 최대 페이지 번호")
   private Integer totalPageNumbers;
+  @ApiModelProperty(notes = "결과에 해당하는 전체 데이터 개수")
   private Long totalElements;
 }
--- src/main/java/team_mic/here_and_there/backend/search/dto/response/ResSearchResultListDto.java
+++ src/main/java/team_mic/here_and_there/backend/search/dto/response/ResSearchResultListDto.java
@@ -1,6 +1,7 @@
 package team_mic.here_and_there.backend.search.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,12 +11,21 @@ import team_mic.here_and_there.backend.common.PageMetaDataDto;
 @Getter
 @JsonPropertyOrder({"language", "searchKeyword", "type", "resultSize", "resultList", "pageMetaDataDto"})
 public class ResSearchResultListDto {
+  @ApiModelProperty(notes = "언어 버전")
   private String language;
+
+  @ApiModelProperty(notes = "사용자 검색 키워드")
   private String searchKeyword;
+
+  @ApiModelProperty(notes = "검색 결과의 type")
   private String type;
 
+  @ApiModelProperty(notes = "검색 결과 리스트의 size")
   private Integer resultSize;
+
+  @ApiModelProperty(notes = "검색 결과 리스트")
   private List resultList;
 
+  @ApiModelProperty(notes = "페이지네이션 메타정보")
   private PageMetaDataDto pageMetaDataDto;
 }
